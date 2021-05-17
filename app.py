a534b991f92333c51576986ce210cbc2a069545c
Yuvalabs <82631723+Yuvalabs@users.noreply.github.com>
2021-05-17 23:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 def basic():
     if request.method == 'POST':
         upload = request.files['upload']
-        storage.child("images/new.mp4").put(upload)
+        storage.child("Images/new.png").put(upload)
         return redirect(url_for('uploads'))
     return render_template('index.html')
 
@@ -40,7 +40,7 @@ def uploads():
     if request.method == 'POST':
         return redirect(url_for('basic'))
     if True:
-        links = storage.child('images/new.mp4').get_url(None)
+        links = storage.child('Images/new.png').get_url(None)
         return render_template('upload.html', l=links)
     return render_template('upload.html')
 
